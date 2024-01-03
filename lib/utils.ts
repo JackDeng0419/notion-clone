0f04e3e59b903364b7101243abe00f0ba2697d84
Jack Deng <jack.z.deng@outlook.com>
2024-01-03 16:38:24 +1030

    init

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+/* trunk-ignore-all(prettier) */
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
