4b47bed9922bd9b2ac6ce676e33cd4244aac0637
Jack Deng <jack.z.deng@outlook.com>
2024-01-04 21:02:07 +1030

    search command

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -6,7 +6,10 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-gray-300", className)}
+      className={cn(
+        "animate-pulse rounded-md bg-primary/5 dark:bg-primary/7",
+        className,
+      )}
       {...props}
     />
   );
